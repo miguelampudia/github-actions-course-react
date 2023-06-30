9adf4941bf92e7e4751f877c1a6ddb28c8ba634f
Miguel Angel <miguel.ampudia@outlook.es>
2023-06-30 18:41:17 -0500

    fix: prueba

--- release.config.js
+++ release.config.js
@@ -1,8 +1,9 @@
 module.exports = {
 	branches: "master",
-	repositoryUrl: "https://github.com/alialaa/react-app",
+	repositoryUrl: "https://github.com/miguelampudia/github-actions-course-react",
 	plugins: [
-		"@semantic-release/commit-analyzer", "@semantic-release/release-notes-generator", 
+	  "@semantic-release/commit-analyzer",
+	  "@semantic-release/release-notes-generator",
 		["@semantic-release/github", {
 			assets: [
 				{ path: "build.zip", label: "Build" },
