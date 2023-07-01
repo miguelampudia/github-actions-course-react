783d3f8b8c7d0a3b0e135e0273f724203bdbe2e3
Miguel Angel <miguel.ampudia@outlook.es>
2023-06-30 22:52:14 -0500

    fix: update comando

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -13,7 +13,7 @@ echo "::error ::Error Message"
 echo "::add-mask::$1"
 echo "Hello $1"
 time=$(date)
-echo "::set-output name=time::$time"
+run: echo "time=$time" >> $GITHUB_OUTPUT
 
 echo "::group::Some expandable logs"
 echo 'some stuff'
