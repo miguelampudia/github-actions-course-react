f901b12b250535452fb231f0c4a262b8a3ba2e62
Miguel Angel <miguel.ampudia@outlook.es>
2023-06-30 21:33:19 -0500

    fix: add mor

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,14 +2,24 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+
+    core.debug("Debug message");
+    core.warning("Warning message");
+    core.error("Error message");
+
 	// throw( new Error("some error message"));
 	const name = core.getInput('who-to-greet');
+    core.setSecret(name);
 	console.log(`Hello ${name}`);
 
 	const time = new Date();
-	core.setOutput("time", time.toTimeString());
+    core.setOutput("time", time.toTimeString());
 
-	console.log(JSON.stringify(github, null, '\t'));
+    core.startGroup("Logging github object");
+    console.log(JSON.stringify(github, null, "\t"));
+    core.endGroup();
+
+    core.exportVariable("HELLO", "hello");
 } catch(error) {
 	core.setFailed(error.message);
 }
